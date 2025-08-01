c8439a4b977af6b057df44315f5c1fe18cc7a4d9
MandulaSiddardha <145456488+MandulaSiddardha@users.noreply.github.com>
2025-08-01 23:41:33 +0530

    Create rectangle.java

---
+++ rectangle.java
@@ -0,0 +1,8 @@
+class rectangle {
+    public static void main(String[] args) {
+       int length = 6;
+       int breadth = 4;
+       int area = length*breadth;
+       System.out.println("area of rectangle is :"+area);
+    }
+}
