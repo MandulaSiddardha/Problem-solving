83780b8400b3294558cb51e5cef4911da3f2efeb
MandulaSiddardha <145456488+MandulaSiddardha@users.noreply.github.com>
2025-08-01 23:58:03 +0530

    Create perimeterrectangle.java

---
+++ perimeterrectangle.java
@@ -0,0 +1,8 @@
+class perimeter{
+    public static void main(String[] args) {
+       int length = 5;
+       int breadth = 3;
+       int Perimeter = 2 * (length + breadth) ;
+       System.out.println("perimeter of rectangle is :" + Perimeter);
+    }
+}
