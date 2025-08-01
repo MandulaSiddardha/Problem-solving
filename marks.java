b53d500440e01f2a4ad9634b85ec810fc9de12a6
MandulaSiddardha <145456488+MandulaSiddardha@users.noreply.github.com>
2025-08-02 00:32:51 +0530

    Create marks.java

---
+++ marks.java
@@ -0,0 +1,10 @@
+class marks{
+    public static void main(String[] args) {
+       int maths=85;
+       int physics=90;
+       int chemistry=88;
+       int totalmarks= maths+physics+chemistry;
+       
+        System.out.println("total marks is :"+ totalmarks);
+    }
+}
