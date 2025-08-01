4d25f7ce1ed4d7c893e0ce7bcaaaea3acfa323d2
MandulaSiddardha <145456488+MandulaSiddardha@users.noreply.github.com>
2025-08-01 23:50:18 +0530

    Create triangle.java

---
+++ triangle.java
@@ -0,0 +1,9 @@
+
+class triangle{
+    public static void main(String[] args) {
+       double base = 8;
+       double height = 5;
+       double area = (base*height)/2;
+       System.out.println("area of triangle is :"+area);
+    }
+}
