e4a76756ac6f17a768abc90d241f9f5370702975
MandulaSiddardha <145456488+MandulaSiddardha@users.noreply.github.com>
2025-08-02 00:39:28 +0530

    Create averagemarks.java

---
+++ averagemarks.java
@@ -0,0 +1,10 @@
+class marks{
+    public static void main(String[] args) {
+       int maths=85;
+       int physics=90;
+       int chemistry=88;
+       double averagemarks= (maths+physics+chemistry)/3.0;
+       
+        System.out.printf("average marks is :%.2f\n", averagemarks);
+    }
+}
