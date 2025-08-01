9ac040d19fe52e69a22efe898db2fa009387a1c4
MandulaSiddardha <145456488+MandulaSiddardha@users.noreply.github.com>
2025-08-01 23:53:37 +0530

    Create perimeter.java

---
+++ perimeter.java
@@ -0,0 +1,7 @@
+class perimeter{
+    public static void main(String[] args) {
+       int side = 6;
+       int perimeter = 4*side;
+       System.out.println("perimeter of square is :" + perimeter);
+    }
+}
